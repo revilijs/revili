dc4637f684d3dfd0f04a6852e169b564adf44b00
reco_luan <recoluan@qq.com>
2023-02-08 00:21:57 +0800

    docs: update docs

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -3,8 +3,8 @@ import recoTheme from 'vuepress-theme-reco'
 import {themeConfig} from './config/index'
 
 export default defineUserConfig({
-  title: 'Vili',
-  base: '/',
+  title: 'Vicli',
+  base: '/vicli/',
   description: '一款基于 Vue3、Vite 的脚手架模块化解决方案。',
   theme: recoTheme({...themeConfig}),
   // debug: true,
---
+++ scripts/docs-deploy.sh
@@ -0,0 +1,19 @@
+cd docs
+pnpm build
+cd .vuepress/dist
+
+git init
+git add -A
+git commit -m 'docs: update docs'
+
+# git push -f git@github.com:recoluan/vuepress-reco-doc.git master
+
+# git push -f git@git.coding.net:recoluan/vuepress-theme-reco-doc.git master
+
+git remote remove origin
+git remote add origin git@github.com:recoluan/vicli.git
+git remote -v
+git push origin master:pages-code -f
+
+cd ../
+rm -rf dist
