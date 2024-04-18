a39c07788f2f4c59cb6e35d7d50b64eea9298fa0
Reco <recoluan@qq.com>
2024-04-17 21:27:58 -0500

    Update config.ts

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,10 +1,12 @@
 import {defineUserConfig} from 'vuepress'
 import recoTheme from 'vuepress-theme-reco'
+import { viteBundler } from '@vuepress/bundler-vite'
 import {themeConfig} from './config/index'
 
 export default defineUserConfig({
   title: 'Revili',
   description: '一款基于 Vue3、Vite 的脚手架模块化解决方案。',
+  bundler: viteBundler({}),
   theme: recoTheme({...themeConfig}),
   // debug: true,
 })
