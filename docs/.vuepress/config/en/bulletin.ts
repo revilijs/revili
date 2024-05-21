e5bbb4bbcd8b814ce59faee50efb2141b4dc48e0
reco_luan <recoluan@qq.com>
2024-05-22 00:58:10 +0800

    docs: update docs

--- docs/.vuepress/config/en/bulletin.ts
+++ docs/.vuepress/config/en/bulletin.ts
@@ -2,12 +2,24 @@ export const bulletin = {
   body: [
     {
       type: 'title',
-      content: 'Welcome to try it. Please add Wechat for communication:',
+      content: 'Latest Version',
+    },
+    {
+      type: 'text',
+      content: `
+      <ul>
+        <li>revili: 0.0.1-alpha.10</li>
+        <li>@revili/revili-kit-demo: 0.0.1-alpha.8</li>
+      </ul>`,
+      style: 'font-size: 12px; color: #5D67E8;'
+    },
+    {
+      type: 'title',
+      content: 'QQ Channel: 3162zhdo3o',
     },
     {
       type: 'image',
-      src: `https://img.shields.io/badge/wechat-reco__luan-brightgreen.svg?style=flat-square`,
-      style: 'width: 150px',
+      src: '/qq_channel.png',
     },
-  ],
+  ]
 }
--- docs/.vuepress/config/zh/bulletin.ts
+++ docs/.vuepress/config/zh/bulletin.ts
@@ -2,12 +2,24 @@ export const bulletin = {
   body: [
     {
       type: 'title',
-      content: '欢迎试用，沟通交流请加微信:',
+      content: '最新版本',
+    },
+    {
+      type: 'text',
+      content: `
+      <ul>
+        <li>revili: 0.0.1-alpha.10</li>
+        <li>@revili/revili-kit-demo: 0.0.1-alpha.8</li>
+      </ul>`,
+      style: 'font-size: 12px; color: #5D67E8;'
+    },
+    {
+      type: 'title',
+      content: 'QQ 频道：3162zhdo3o',
     },
     {
       type: 'image',
-      src: `https://img.shields.io/badge/wechat-reco__luan-brightgreen.svg?style=flat-square`,
-      style: 'width: 150px',
+      src: '/qq_channel.png',
     },
-  ],
+  ]
 }
